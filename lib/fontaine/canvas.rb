0b77053f2815ece213588e4712863dd76d7d28d7
JPWetstein <jeep.wetstein@gmail.com>
2013-07-22 12:19:25 -0400

    Fixed some bugs in fontaine.js, and implemented a lot more functions.
    Set up some metaprogramming in canvas.rb to implement most of those functions.

--- lib/fontaine/canvas.rb
+++ lib/fontaine/canvas.rb
@@ -7,6 +7,8 @@ class Canvas
   attr_accessor :html_options
   attr_accessor :settings
   
+  
+  
   def initialize(id, width, height, alt = "", html_options = {})
     @id = id
     @width = width
@@ -76,6 +78,14 @@ class Canvas
     @on_response.call(params) if defined? @on_response
   end
   
+  def fill_style(style)
+    if style.is_a? String
+      send_msg "fillStyle #{style}"
+    else
+      puts "NOT IMPLEMENTED YET" #TODO: Implement gradiants and patterns
+    end
+  end
+  
   def stroke_style(style)
     if style.is_a? String
       send_msg "strokeStyle #{style}"
@@ -84,13 +94,66 @@ class Canvas
     end
   end
   
-  def stroke_rectangle(x, y, width, height)
-    send_msg "strokeRect #{x} #{y} #{width} #{height}"
-  end
+  # def stroke_rect(x, y, width, height)
+    # send_msg "strokeRect #{x} #{y} #{width} #{height}"
+  # end
   
   def send_msg(msg)
+    puts msg
     @settings.sockets.each{|s| s.send(msg)} if defined? @settings
   end
+  
+  def method_missing(method_sym, *arguments, &block)
+    if method_array.include? method_sym.to_s
+      puts "yes"
+      send_msg "#{rubyToJsCommand(method_sym)} #{arguments.join(" ")}"
+    else
+      puts method_sym
+      super(method_sym, *arguments, &block)
+    end
+    #super(method_sym, *arguments, &block)
+  end
+  
+  def method_array
+    return [
+      #Colors, Styles, and Shadows
+      #TODO: fill_style, stroke_style, shadow_color, shadow_blur, shadow_offset_x, shadow_offset_y
+      #TODO: create_linear_gradient, create_pattern, create_radial_gradient, add_color_stop
+      #Line Styles
+      #TODO: line_cap, line_join, line_width, miter_limit
+      #Rectangles
+      "rect", "fill_rect", "stroke_rect", "clear_rect", 
+      #Paths
+      "fill", "stroke", "begin_path", "move_to", "close_path", "line_to", "clip", 
+      "quadratic_curve_to", "bezier_curve_to", "arc", "arc_to", #TODO: isPointInPath
+      #Transformations
+      "scale", "rotate", "translate", "transform", "set_transform",
+      #Text
+      #TODO:font, text_align, text_baseline
+      "fill_text", "stroke_text", "measure_text",
+      #Image Drawing
+      #TODO: draw_image
+      #Pixel Manipulation
+      #TODO: width, height, data, create_image_data, get_image_data, put_image_data
+      #Composting
+      #TODO: global_alpha, global_composite_operation
+      #Other
+      #TODO: save, restore, create_event, get_context, to_data_url
+      ]
+  end
+  
+  def rubyToJsCommand(method_sym)
+    js_command = method_sym.to_s
+    js_commands = js_command.split('_')
+    if js_commands.size > 1
+      js_commands[1..js_commands.size].each do |command|
+        command.capitalize!
+      end
+    end
+    js_commands.join
+  end
+  
+  
   
 end
 end
