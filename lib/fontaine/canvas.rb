e81aea09134eb5c3cd331fcac5326c413a1f2dd6
JPWetstein <jeep.wetstein@gmail.com>
2013-08-02 06:44:06 -0400

    Added a pseudo-response for methods that are supposed to return attributes. Doesn't work for methods that measure, but works if you set the attributes through the gem.

--- lib/fontaine/canvas.rb
+++ lib/fontaine/canvas.rb
@@ -6,8 +6,10 @@ class Canvas
   attr_accessor :alt
   attr_accessor :html_options
   attr_accessor :settings
-  attr_accessor :last_response
   attr_accessor :ws
+  attr_accessor :last_response
+  attr_accessor :attributes
+  
   
   def initialize(id, width, height, alt = "", html_options = {})
     @id = id
@@ -15,25 +17,49 @@ class Canvas
     @height = height
     @alt = alt
     @html_options = html_options
+    @attributes = {
+      :fill_style => '#000000', 
+      :stroke_style => '#000000',
+      :shadow_color => '#000000',
+      :shadow_blur => 0,
+      :shadow_offset_x => 0,
+      :shadow_offset_y => 0,
+      :line_cap => 'butt',
+      :line_join => 'miter',
+      :line_width => '1',
+      :miter_limit => '10',
+      :font => '10px sans-serif',
+      :text_align => 'start',
+      :text_baseline => 'alphabetic',
+      :global_alpha => 1.0,
+      :global_composite_operation => 'source-over'
+      
+      }
   end
   
   def start(request, settings, host = 'localhost', port = 4567, &block)  
     @settings = settings 
-    block.call
+    
+    
     request.websocket do |ws|
+      
       ws.onopen do
+        block.call
         @ws = ws
         @ws.send("register ##{id}")
         @settings.sockets << ws
+        
       end
       ws.onmessage do |msg|
-        puts("recieved message: #{msg}")
+        #puts("recieved message: #{msg}")
+        #EM.next_tick { process_message(msg)}
         process_message(msg)
       end
       ws.onclose do
         @settings.sockets.delete(ws)
       end
     end
+    
   end
   
   def process_message(s_message)
@@ -51,11 +77,11 @@ class Canvas
     when "mouseup"
       trigger_on_mouseup(params["x"], params["y"], params["button"])
     when "mouseover"
-      trigger_on_mouseover(params["x"], params["y"], params["button"]) 
+      trigger_on_mouseover(params["x"], params["y"]) 
     when "mouseout"
-      trigger_on_mouseout(params["x"], params["y"], params["button"]) 
+      trigger_on_mouseout(params["x"], params["y"]) 
     when "mousemove"
-      trigger_on_mousemove(params["x"], params["y"], params["button"])
+      trigger_on_mousemove(params["x"], params["y"])
     when "touchstart"
       trigger_on_touchstart(params["x"], params["y"], params["id"])  
     when "touchmove"
@@ -67,13 +93,12 @@ class Canvas
     when "keydown"
       trigger_on_keydown(params["key_code"])
     when "keypress"
-      trigger_on_keypress(params["key_code"])    
-        
-    when "response"
-      @last_response =  params.flatten.to_s
-      trigger_on_response(@last_response)
+      trigger_on_keypress(params["which"])
     when "click"
-      trigger_on_click(params["x"], params["y"], params["button"])
+      trigger_on_click(params["x"], params["y"], params["button"])      
+    when "response"
+      @last_response =  params.flatten[0].to_s
+      trigger_on_response(@last_response)
     else
       puts "unimplemented method #{s_command}"
     end
@@ -87,22 +112,26 @@ class Canvas
     return "<canvas id=\"#{@id}\" width=\"#{@width}\" height=\"#{@height}\" #{options}>#{@alt}</canvas>"
   end
 
-  def fill_style(style)
+  def fill_style(style="", &blk)
     if style.is_a? String
       send_msg "fillStyle #{style}"
     else
       send_msg "fillStyleObject #{style.id}"
     end
-    return @last_response
+    
+    # return @last_response
+    return @attributes[fill_style]
+    
   end
   
-  def stroke_style(style)
+  def stroke_style(style="")
     if style.is_a? String
       send_msg "strokeStyle #{style}"
     else
       send_msg "strokeStyleObject #{style.id}"
     end
-    return @last_response
+    # return @last_response
+    return @attributes[stroke_style]
   end
   
   def create_linear_gradient(x0, y0, x1, y1, id)
@@ -124,19 +153,19 @@ class Canvas
     return image_data.new(id, self, args)
   end
   
-  def get_image_data(id, x, y, width, height)
-    send_msg("getImageData #{x} #{y} #{width} #{height} #{id}")
-    return image_data.new(id, self)
-  end
+  # def get_image_data(id, x, y, width, height)
+    # send_msg("getImageData #{x} #{y} #{width} #{height} #{id}")
+    # return image_data.new(id, self)
+  # end
   
   def put_image_data(image, x, y, dirty_x="", dirty_y="", dirty_width="", dirty_height="")
     send_msg("putImageData #{image.id} #{x} #{y} #{dirty_x} #{dirty_y} #{dirty_width} #{dirty_height}")
   end
   
   def send_msg(msg)
-    puts "Sending message: #{msg}"
-    #EM.next_tick {@settings.sockets.each{|s| s.send(msg)} if defined? @settings}
-    EM.next_tick {@ws.send(msg)} if defined? @ws
+    #puts "Sending message: #{msg}"
+    #EM.next_tick {@ws.send(msg)} if defined? @ws
+    @ws.send(msg) if defined? @ws
   end
   
   def method_missing(method_sym, *arguments, &block)
@@ -149,7 +178,8 @@ class Canvas
       send_msg "#{ruby_to_js_command(method_sym)} #{arguments.join(" ")}"
     elsif return_method_array.include? method_sym.to_s
       send_msg "#{ruby_to_js_command(method_sym)} #{arguments.join(" ")}"
-      return @last_response  
+      @attributes[method_sym] = arguments[0] if (!arguments[0].nil? && !@attributes[method_sum].nil?)
+      return @attributes[method_sym]
     else
       super(method_sym, *arguments, &block)
     end
